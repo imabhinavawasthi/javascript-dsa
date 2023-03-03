71b5ebe973ee3434acd51d059c49e67bbe150cb4
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-03-03 19:51:52 +0530

    frequency counter

---
+++ Frequency Counter/anagrampalindrome.js
@@ -0,0 +1,26 @@
+let s="ABOBANOCCY";
+
+var mp=new Map()
+
+for(let i=0;i<s.length;i++){
+    let x=s[i]
+    if(mp.has(x)){//already present
+        mp.set(x,mp.get(x)+1)
+    }
+    else{//not present
+        mp.set(x,1);
+    }
+}
+let c=0;
+mp.forEach((value,key)=>{
+    if(value%2==1){
+        c++;
+    }
+})
+
+if(c>1){
+    console.log("NO");
+}
+else{
+    console.log("YES");
+}
