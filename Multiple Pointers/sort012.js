46658cf5bd69ad40fcfe9d86ad3ed0c036d33ea1
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-03-18 11:06:38 +0530

    sort012

---
+++ Multiple Pointers/sort012.js
@@ -0,0 +1,21 @@
+let arr=[2,0,0,1,0,1,0,2,0,1,2,0,1,2,1,0,2,1,0]
+let n=arr.length
+let l=0,r=n-1,i=0
+while(i<=r){
+    if(arr[i]==2){
+        //swap with r
+        let temp=arr[i]
+        arr[i]=arr[r]
+        arr[r]=temp
+        r--
+    }
+    else if(arr[i]==0){
+        let temp=arr[i]
+        arr[i]=arr[l]
+        arr[l]=temp
+        l++
+        i++
+    }
+    else i++
+}
+console.log(arr);
