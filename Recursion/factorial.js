15cded4e9a7ece5413aa8db47b5ec8a4e890cc81
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-03-20 20:22:12 +0530

    recursive

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -2,7 +2,8 @@ let n=6
 
 function fac(n){
     //base
-    if(n<=0)return 1
+    if(n==1)return 1
+
     //recursive
     return n*fac(n-1)
 }
---
+++ Recursion/tailrecusion.js
@@ -0,0 +1,12 @@
+function fun(n){
+    //base
+    if(n==0)return
+
+    //recursive
+    console.log(n);
+    fun(n-1)
+}
+
+fun(5)
+
+//5 4 3 2 1 
