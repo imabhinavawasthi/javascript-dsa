488039866ea2487e3c74c15e64533b296dd221f7
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-03-28 19:32:26 +0530

    leetcode

---
+++ Problems/reversestringII.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+ var reverseStr = function(str, k) {
+    let i=0
+    var s=str.split("")
+    var n=s.length
+    while(i<n){
+        let l=i
+        let r=i+k-1
+        if(r>n)r=n-1
+        while(l<r){
+            s[l]=s[l].concat(s[r])
+            s[l]=s[l].slice(0,s[l].length-s[r].length)
+            s[l]=s[l].slice(s[r].length)
+            l++
+            r--
+        }
+        i+=2*k
+    }
+    return s.join("")
+};
