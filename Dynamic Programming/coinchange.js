00546f353479d0c9a010b3ee04394f9815196f3a
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-03-30 13:03:17 +0530

    dp leetcode

---
+++ Dynamic Programming/coinchange.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+ var coinChange = function(coins, amount) {
+    n=coins.length
+    var dp=new Array(n+1)
+    for(let i=0;i<=n;i++){
+        dp[i]=new Array(amount+1)
+    }
+    var rec=function(i, coins, amount){
+        if(i==0){
+            if(amount<0){
+                return 1e9;
+            }
+            else if(amount==0){
+                return 0
+            }
+            else{
+                if(amount%coins[0]==0){
+                    return amount/coins[0]
+                }
+                else{
+                    return 1e9;
+                }
+            }
+        }
+        if(dp[i][amount])
+        return dp[i][amount]
+        var nottake=rec(i-1,coins,amount)
+        var take=1e9
+        if(amount>=coins[i]){
+            take=rec(i,coins,amount-coins[i])+1
+        }
+        return dp[i][amount]=Math.min(nottake,take)
+    }
+    
+    if(rec(n-1,coins,amount)==1e9)return -1
+    return rec(n-1,coins,amount)
+};
--- pending.md
+++ pending.md
@@ -2,3 +2,5 @@ https://leetcode.com/problems/combination-sum/description/
 
 https://leetcode.com/discuss/interview-question/372434
 
+https://leetcode.com/problems/maximum-swap/description/
+
