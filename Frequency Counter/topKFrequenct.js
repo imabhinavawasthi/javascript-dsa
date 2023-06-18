4c3c46cb33de17ccaa53888de02b55abbf8f7012
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-06-18 13:52:22 +0530

    check 4

--- Frequency Counter/topKFrequenct.js
+++ Frequency Counter/topKFrequenct.js
@@ -12,10 +12,10 @@ var topKFrequent = function(nums, k) {
     }
     console.log(Object.entries(map));
     console.log(Object.entries(map).sort((a,b)=>a[1]-b[1]));
-    // console.log(Object.entries(map).sort((a,b)=>a[1]-b[1])
-    // .slice(-k));
-    // console.log(Object.entries(map).sort((a,b)=>a[1]-b[1])
-    // .slice(-k).map((item)=>item[0]));
+    console.log(Object.entries(map).sort((a,b)=>a[1]-b[1])
+    .slice(-k));
+    console.log(Object.entries(map).sort((a,b)=>a[1]-b[1])
+    .slice(-k).map((item)=>item[0]));
 }
 
 topKFrequent([5,2,1,5,2,1,8,2,1,2,1,1,3,3],2)
