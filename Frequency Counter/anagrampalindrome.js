2d5868558eda6522448e7f97609c111467aa98f6
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-06-18 13:20:21 +0530

    anagram palindrome

--- Frequency Counter/anagrampalindrome.js
+++ Frequency Counter/anagrampalindrome.js
@@ -1,21 +1,24 @@
-let s="ABOBANOCCY";
+let s="abbaybb"
+
+let n=s.length
 
 var mp=new Map()
 
-for(let i=0;i<s.length;i++){
+for(let i=0;i<n;i++){
     let x=s[i]
-    if(mp.has(x)){//already present
+    if(mp.has(x)){
         mp.set(x,mp.get(x)+1)
     }
-    else{//not present
-        mp.set(x,1);
+    else{
+        mp.set(x,1)
     }
 }
 
-let c=0;
+let c=0
+
 mp.forEach((value,key)=>{
-    if(value%2==1){//odd
-        c++;
+    if(value%2==1){
+        c++
     }
 })
 
